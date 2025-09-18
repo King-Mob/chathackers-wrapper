2ce273d2eda9fbe2e459cb88ba87eb946fefcb1c
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-09-18 16:24:36 +0100

    update links

--- web/Home.tsx
+++ web/Home.tsx
@@ -1,17 +1,33 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 
 export default function Home() {
-    return <div>
-        <p>To determine how we communicate is a foundational democratic right.
-            Huge chat platforms have become the de facto public square.
-            Now you can adapt how the square works to suit your needs.</p>
-        <h2>Get Started</h2>
-        <p>WhatsApp: add +44 7724 736427 to your group</p>
-        <p>Then use emoji reacts and replies to select tools and use them.</p>
-        <Link to="tools"><p>See available tools</p></Link>
-        <Link to="faq"><p>FAQ</p></Link>
-        <Link to="legal"><p>Legal</p></Link>
-        <Link to="volunteer"><p>Volunteer</p></Link>
-        <p>Chat Hackers is a collaboration of Campaign Lab and John Evans.</p>
+  return (
+    <div>
+      <p>
+        To determine how we communicate is a foundational democratic right. Huge
+        chat platforms have become the de facto public square. Now you can adapt
+        how the square works to suit your needs.
+      </p>
+      <h2>Get Started</h2>
+      <p>WhatsApp: add +44 7724 736427 to your group</p>
+      <p>Then use emoji reacts and replies to select tools and use them.</p>
+      <Link to="tools">
+        <p>See available tools</p>
+      </Link>
+      <Link to="faq">
+        <p>FAQ</p>
+      </Link>
+      <Link to="legal">
+        <p>Legal</p>
+      </Link>
+      <Link to="volunteer">
+        <p>Volunteer</p>
+      </Link>
+      <p>
+        Chat Hackers is a collaboration of{" "}
+        <Link to="https://campaignlab.uk">Campaign Lab</Link> and{" "}
+        <Link to="https://john.spacetu.be">John Evans</Link>.
+      </p>
     </div>
+  );
 }
